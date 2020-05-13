2bb15b4db282df7aec81bd3a800ad885d38f0d57
wangk-sy <1638869567@qq.com>
2020-05-13 18:14:02 +0800

    vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -80,7 +80,7 @@
             { required: true, message: "请输入用户名", trigger: "blur" }
           ],
           password: [{ required: true, message: "请输入密码", trigger: "blur" }],
-          code: [{required: true, message: '请输入验证码', trigger: 'blur'}]
+          code: [{required: true, message: '请输入验证码1', trigger: 'blur'}]
         }
       };
     },
