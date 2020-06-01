c0b589025849751a2eae27817270d0bb53d9e5c0
wangk-sy <1638869567@qq.com>
2020-06-01 17:44:26 +0800

    commit success

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="hello">
-    <el-header class="homeHeader">
-      <div class="title">Lch</div>
-    </el-header>
-    <el-row :gutter="10">
-      <el-col :xs="4" :sm="4" :md="4" :lg="4">
+    <el-container>
+      <el-header class="homeHeader">
+        <div class="title">Lch</div>
         <div>
-          <el-menu  class="el-menu-vertical-demo" style="min-height:900px" @select="handleSelect">
+          <el-dropdown>
+          <span class="el-dropdown-link">
+              下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人中心</el-dropdown-item>
+              <el-dropdown-item>设置</el-dropdown-item>
+              <el-dropdown-item divided>注销登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px;">
+          <el-menu class="el-menu-vertical-demo" style="min-height:900px" @select="handleSelect">
             <el-menu-item index="1"><i class="fa fa-chrome"></i>网址浏览</el-menu-item>
             <el-menu-item index="2"><i class="fa fa-th-list"></i>网址管理</el-menu-item>
           </el-menu>
-        </div>
-      </el-col>
-      <el-col :xs="20" :sm="20" :md="20" :lg="20">
-        <div style="margin-top:10px">
-          <router-view></router-view>
-        </div>
-      </el-col>
-    </el-row>
+        </el-aside>
+        <el-main>
+          <el-breadcrumb separator-class="el-icon-arrow-right" v-if="this.$router.currentRoute.path!=='/home'">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>{{this.$router.currentRoute.name}}</el-breadcrumb-item>
+          </el-breadcrumb>
+          <div class="homeWelcome" v-if="this.$router.currentRoute.path==='/home'">
+            欢迎来到Lch系统
+          </div>
+          <router-view class="homeRouterView"/>
+        </el-main>
+      </el-container>
+
+    </el-container>
   </div>
 </template>
 
@@ -25,10 +43,7 @@
   export default {
     name: 'HelloWorld',
     data () {
-      return {
-        msg: 'Welcome to Your Vue.js App',
-        searchCriteria: '',
-      }
+      return {}
     },
     methods: {
       handleSelect (key, keyPath) {
@@ -61,5 +76,11 @@
     font-family: 华文行楷;
     color: #ffffff
   }
-
+  .homeWelcome {
+    text-align: center;
+    font-size: 30px;
+    font-family: 华文行楷;
+    color: #409eff;
+    padding-top: 50px;
+  }
 </style>
